a00a849980b746df17b30dfbaeef19b04f5fe2db
Denis Filonov <dennis@newcad.se>
2024-05-22 13:54:50 +0200

    refactor: update page title to "Northern Digital Garden"

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Dev I Norr",
+    pageTitle: "Northern Digital Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -88,4 +88,4 @@ const config: QuartzConfig = {
   },
 }
 
-export default config
+export default config;
