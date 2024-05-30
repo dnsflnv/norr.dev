810c4a345007d9be81c77ed684c267d598897344
Denis Filonov <dennis@newcad.se>
2024-05-30 14:00:46 +0200

    Merge branch 'main' of github.com:zaydek/zaydek.github.io into main

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -20,6 +20,15 @@ export default (() => {
 
     return (
       <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BGN0QWWTVG"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-BGN0QWWTVG');
+        </script>
         <title>{title}</title>
         <meta charSet="utf-8" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
