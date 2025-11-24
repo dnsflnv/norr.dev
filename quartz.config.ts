dccaa95e28c8673346a0dcf7f9a1aa9e4975880d
Dennis <dennis@norr.dev>
2025-11-24 11:09:34 +0100

    chore: Update base URL from quartz.jzhao.xyz to norr.dev

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "norr.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
